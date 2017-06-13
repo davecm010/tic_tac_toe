2a3a19ee98155064deabf97ff7caf065ddb4ce1e
David Moylan <davecm010@gmail.com>
2017-06-13 10:51:31 -0500

    created a functioning game board that updates with user input

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,25 +1,26 @@
 class TicTacToe
-  attr_accessor(:game_board, :turn_counter, :winning_combos)
+  attr_accessor(:game_board, :winning_combos, :used_values)
   def initialize
-    @game_board = [["|_1_|_2_|_3_|"], ["|_4_|_5_|_6_|"], ["|_7_|_8_|_9_|"]]
-    @turn_counter = 0
+    @game_board = ["|_1_|_2_|_3_|", "|_4_|_5_|_6_|", "|_7_|_8_|_9_|"]
     @winning_combos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+    @used_values = []
   end
 
   def new_game
     puts "Tic-Tac-Toe"
     player_1 = Player.new("player_1", "X")
     player_2 = Player.new("player_2", "O")
-    for i in 0..5
-      player_decision(player_1)
-      player_decision(player_2)
+    5.times do
+      player_turn(player_1)
+      player_turn(player_2)
     end
   end
 
   def update_board(user_input, player)
     @game_board.each do |row|
+      row_num = @game_board.index(row)
       if row.include? user_input
-        row.gsub(user_input, player.symbol)
+        @game_board[row_num] = row.gsub(user_input, player.symbol)
       end
     end
   end
@@ -28,17 +29,18 @@ class TicTacToe
     @game_board.each {|row| puts row}
   end
 
-  def turn_counter
-    @turn_counter += 1
-  end
-
-  def player_decision(player)
+  def player_turn(player)
     show_board
     puts "#{player.name} pick a square:"
     user_input = gets.chomp
-    player.add_choices(user_input)
+    until user_input.to_i <= 9 && user_input.to_i > 0
+      puts "The value given is either not an integer,too large,or already in use."
+      puts "Please enter a new value."
+      user_input = gets.chomp
+    end
+    @used_values << user_input
+    player.add_choices(user_input.to_i)
     update_board(user_input, player)
-    turn_counter
   end
 end
 
